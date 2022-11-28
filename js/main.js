e85a4cf2bcbd5a7f9ed2eb89af03d792f044b4f4
TazK7 <khantazz07@gmail.com>
2022-11-27 19:48:18 -0500

    Added a Div inside the carousel-slides class

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     let body = document.querySelector(`body`);
     let script = document.createElement(`script`);
-
     script.setAttribute(`src`, `json/data.json`);
     body.append(script);
+
 }
 
 function bands(json) {
@@ -11,5 +11,14 @@ function bands(json) {
     let anchorTwo = document.getElementsByTagName(`a`)[1];
     anchorOne.classList.add(`hidden-button`, `left-button`);
     anchorTwo.classList.add(`button`, `right-button`);
+    
+    let insert = document.querySelector(`.carousel-slides`);
+    insert.innerHTML = `<div class="slide"></div>`;
+    let check = document.querySelector(`.slide`);
+    console.log(check);
+    //let leftBox = document.createElement(`div`);
+    //leftBox.classList.add(`left-rectangle`);
+    //insert.after(`<div>`);
+
 
 }
