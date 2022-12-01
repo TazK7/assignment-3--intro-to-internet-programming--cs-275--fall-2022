b7f15208562803075bdae0f284f0edbbdd36d049
TazK7 <khantazz07@gmail.com>
2022-12-01 03:08:28 -0500

    *Created For Loop*
    Created a For Loop instead of hardcoding each property from Json File(per teacher suggestion). Removed unnecessary code and cleaned/explained code with comments. Initialized many new variables.

--- js/main.js
+++ js/main.js
@@ -1,72 +1,45 @@
 function bands(json) {
-    // This section of the code is getting the two anchor elements and giving them a class for functionality later
-    let anchorOne = document.getElementsByTagName(`a`)[0];
-    let anchorTwo = document.getElementsByTagName(`a`)[1];
-    anchorOne.classList.add(`hidden-button`, `left-button`);
-    anchorTwo.classList.add(`button`, `right-button`);
-
-    //Initializing a variable to select the carousel-slides div class and insert more divs inside of it
-    let insert = document.querySelector(`.carousel-slides`);
-    insert.innerHTML = `<div class="slide"></div><div class="slide-two"></div><div class="slide-three"></div><div class="slide-four"></div>`;
-
-    //Starting to lay the first contents from the Json file onto the viewport
-    let select = document.querySelector(`.slide`);
+    //This section of the code is declaring global variables & adding classes to elements
+    let insert = document.querySelectorAll(`div`)[1];
     let content = ``;
 
-    content += `<p>${json[0].artist}</p>`;
-    content += `<p>${json[0].album}</p>`;
-    content += `<img src=${json[0].cover_image.path}>`;
-    content += `<p>${json[0].review.content}</p>`;
-    content += `<p><a href ="${json[0].review.url}"</a>—${json[0].review.source}</p>`;
+    //Creating variables that store the anchor elements in nav and give them a class
+    let anchorOne = document.getElementsByTagName(`a`)[0];
+    let anchorTwo = document.getElementsByTagName(`a`)[1];
+    anchorOne.classList.add(`larrow`);
+    anchorTwo.classList.add(`rarrow`);
 
-    select.innerHTML = content;
+    //Creating For loop to run through Json Data and Displaying it as Prof.Roy stated I should do in class
+    for(let i=0; i<json.length; i++){
+        content += `<div class="slide">`
+        content += `<h2 class="middle">${json[i].album}</h2>`;
+        content += `<p class="middle space"><a href="${json[i].url}">${json[i].artist}</a></p>`;
+        content += `<img src=${json[i].cover_image.path}>`;
+        content += `<p class="middle">Credit: <a href="${json[i].cover_image.url}" class="align">${json[i].cover_image.credit}</a><p>`;
+        content += `<p>${json[i].review.content}</p>`;
+        content += `<a href ="${json[i].review.url}"</a>—${json[i].review.source}`;
+        content += `</div>`
+    }
 
-    //Second set of data from Json file
-    let selectTwo = document.querySelector(`.slide-two`)
-    let contentTwo = ``;
-
-    contentTwo += `<p>${json[1].artist}</p>`;
-    contentTwo += `<p>${json[1].album}</p>`;
-    contentTwo += `<img src=${json[1].cover_image.path}>`;
-    contentTwo += `<p>${json[1].review.content}</p>`;
-    contentTwo += `<p><a href ="${json[1].review.url}"</a>—${json[1].review.source}</p>`;
-
-    selectTwo.innerHTML = contentTwo;
-
-    //Third Set of data from Json File
-    let selectThree = document.querySelector(`.slide-three`)
-    let contentThree = ``;
-
-    contentThree += `<p>${json[2].artist}</p>`;
-    contentThree += `<p>${json[2].album}</p>`;
-    contentThree += `<img src=${json[2].cover_image.path}>`;
-    contentThree += `<p>${json[2].review.content}</p>`;
-    contentThree += `<p><a href ="${json[2].review.url}"</a>—${json[2].review.source}</p>`;
-
-    selectThree.innerHTML = contentThree; 
-
-    //Last set of data from Json File
-    let selectFour = document.querySelector(`.slide-four`)
-    let contentFour = ``;
-
-    contentFour += `<p>${json[3].artist}</p>`;
-    contentFour += `<p>${json[3].album}</p>`;
-    contentFour += `<img src=${json[3].cover_image.path}>`;
-    contentFour += `<p>${json[3].review.content}</p>`;
-    contentFour += `<p><a href ="${json[3].review.url}"</a>—${json[3].review.source}</p>`;
-
-    selectFour.innerHTML = contentFour;
-    
-    
-    //let leftBox = document.createElement(`div`);
-    //leftBox.classList.add(`left-rectangle`);
-    //insert.after(`<div>`);
+    insert.innerHTML = content;
 }
 
 window.onload = () => {
+    //Appending the jsonp file into the tail end of the body
     let body = document.querySelector(`body`);
     let script = document.createElement(`script`);
-
     script.setAttribute(`src`, `json/data.json`);
     body.append(script);
+
+    //Declaring Variables for button function
+    let increment, width = 0;
+    let firBut = document.querySelectorAll(`img`)[0];
+    let secBut = document.querySelectorAll(`img`)[1];
+    let slides = document.querySelector(`.carousel-slides`);
+    let oneSlide = document.querySelectorAll(`.carousel-slides`)[0];
+
+    //Giving Classes to variables for function
+    slides.classList.add(`sliding`);
+    firBut.classList.add(`one-button`, `none`);
+    secBut.classList.add(`two-button`, `none`);
 }
